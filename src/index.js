d0188eb58becdbf07eddca4f115387e2164962e2
braed <hegartyb@iastate.edu>
2024-04-06 12:25:04 -0500

    adding stuff

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@
 *Pop-up payment information
 *Video
 */
+import {App} from './app';
+//import logo from "./logo.png";
+import React, {useState} from "react";
+import {products} from "./products"
+//import {Categories} from "./Categories"
+
+function BrowseView() {
+    return (
+    <div >
+    {products[0].description}
+    </div>
+    );
+    }
+    export default App;
---
+++ src/products.js
@@ -0,0 +1,57 @@
+export const products = [
+    {
+    "id": 1,
+    "title": "Bumblebee",
+    "price": 44.99,
+    "description": "Yellow and Black Checkered Button Down Shirt.",
+    "category": "Shirts",
+    "image": "https://images.pexels.com/photos/1337477/pexels-photo-1337477.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "rating": { "rate": 3.9, "count": 120 }
+  },
+  {
+    "id": 2,
+    "title": "Sea Breeze",
+    "price": 54.99,
+    "description": "Professional Light Blue Collared Button Down.",
+    "category": "Shirts",
+    "image": "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "rating": { "rate": 3.9, "count": 120 }
+  },
+  {
+    "id": 3,
+    "title": "Coke Zero",
+    "price": 22.99,
+    "description": "All Red Casual / Athletic Sneakers.",
+    "category": "Shoes",
+    "image": "https://images.pexels.com/photos/1306248/pexels-photo-1306248.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "rating": { "rate": 3.9, "count": 120 }
+  },
+  {
+    "id": 4,
+    "title": "Maroon Waves",
+    "price": 99.99,
+    "description": "Monotone Maroon Jacket and Pants Suit Combination.",
+    "category": "Dressware",
+    "image": "https://images.pexels.com/photos/1300550/pexels-photo-1300550.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "rating": { "rate": 3.9, "count": 120 }
+  },
+  {
+    "id": 5,
+    "title": "Beach Vibes",
+    "price": 29.99,
+    "description": "Hawaiian Floral Short Sleeve Button Down.",
+    "category": "Resort",
+    "image": "https://images.pexels.com/photos/2897531/pexels-photo-2897531.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "rating": { "rate": 3.9, "count": 120 }
+  },
+  {
+    "id": 6,
+    "title": "Tank Green",
+    "price": 99.99,
+    "description": "Mild Green Two Piece Suit.",
+    "category": "Dressware",
+    "image": "https://images.pexels.com/photos/1321943/pexels-photo-1321943.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "rating": { "rate": 3.9, "count": 120 }
+  }
+  
+]
